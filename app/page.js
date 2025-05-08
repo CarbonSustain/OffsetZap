0997664c8df871ce1243060807048f8d583a1989
pbryzek <bryzek@gmail.com>
2025-05-08 01:09:28 -0700

    updat epage

--- app/page.js
+++ app/page.js
@@ -1,14 +1,6 @@
-import OffsetZapApp from './components/OffsetZapApp';
-import SubAccountControls from './components/SubAccountControls';
+// app/page.js
+import OffsetZapApp from "./components/OffsetZapApp";
 
-export default function Home() {
-  return (
-    <main className="p-4">
-      <OffsetZapApp />
-      <div className="mt-6">
-        <SubAccountControls />
-      </div>
-    </main>
-  );
+export default function Page() {
+  return <OffsetZapApp />;
 }
-
