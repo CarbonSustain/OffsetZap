5be2538bde254f9057e5eac08a37f4aeb6833bf1
pbryzek <bryzek@gmail.com>
2025-05-08 02:06:47 -0700

    import

--- app/components/SubAccountControls.js
+++ app/components/SubAccountControls.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
 import { ethers } from "ethers";
 
