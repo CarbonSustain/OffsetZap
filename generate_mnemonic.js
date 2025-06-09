a1d2c94144bce219d056c0d8e2d3cc5b70b43d1e
HabibDjaroueh <habibdjaroueh@berkeley.edu>
2025-06-08 20:16:47 -0700

    added XMTP mnemonic to .env file

---
+++ generate_mnemonic.js
@@ -0,0 +1,7 @@
+const { Wallet } = require("ethers");
+
+const wallet = Wallet.createRandom();
+
+console.log("Mnemonic:", wallet.mnemonic.phrase);
+console.log("Address:", wallet.address);
+console.log("Private Key:", wallet.privateKey);
